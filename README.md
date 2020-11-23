c8bdcd0a7bdc79b596a2f6cff8019b0eef0bcf57
misiekhardcore <misiekhardcore@gmail.com>
2020-11-23 20:30:05 +0100

    1. setting up server

---
+++ README.md
@@ -0,0 +1,4 @@
+GraphQL API from this [video](https://www.youtube.com/watch?v=7giZGFDGnkc&list=PL55RiY5tL51rG1x02Yyj93iypUuHYXcB_)
+
+1. install `express`
+1. install `nodemon`
---
+++ app.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/',(req,res,next)=>{
+  res.send('hello')
+})
+
+app.listen(4000, () => {
+  console.log("server is running");
+});
