a76215f640b22a008db0cfd93efc0e86a71b2d5d
misiekhardcore <misiekhardcore@gmail.com>
2020-11-24 13:24:31 +0100

    database connection

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-GraphQL API from this [video](https://www.youtube.com/watch?v=7giZGFDGnkc&list=PL55RiY5tL51rG1x02Yyj93iypUuHYXcB_)
+GraphQL API from this [video](https://youtu.be/sOUNgOx0GcU?list=PL55RiY5tL51rG1x02Yyj93iypUuHYXcB_&t=112)
 
 1. install `express`
 1. install `nodemon`
+1. install `express-graphql`
+1. install `graphql`
+1. install `mongoose`
--- app.js
+++ app.js
@@ -1,13 +1,80 @@
-import express from "express";
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
+
+const Event = require("./modules/events");
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/',(req,res,next)=>{
-  res.send('hello')
-})
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+    type Event{
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
 
-app.listen(4000, () => {
-  console.log("server is running");
-});
+    input EventInput{
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
+    type RootQuery{
+      events: [Event!]!
+    }
+
+    type RootMutation{
+      createEvent(eventInput: EventInput): Event
+    }
+
+    schema{
+      query: RootQuery
+      mutation: RootMutation
+    }`),
+    rootValue: {
+      events: () => {
+        return Event.find()
+          .then((events) => events.map((event) => ({ ...event._doc })))
+          .catch((err) => {
+            throw err;
+          });
+      },
+      createEvent: ({ eventInput: { title, description, price, date } }) => {
+        const event = new Event({
+          title: title,
+          description: description,
+          price: price,
+          date: new Date(date),
+        });
+        return event
+          .save()
+          .then((result) => ({ ...result._doc }))
+          .catch((err) => {
+            throw err;
+          });
+      },
+    },
+    graphiql: true,
+  })
+);
+
+mongoose
+  .connect("mongodb://localhost:27017/events", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(4000, () => {
+      console.log("server is running");
+    });
+  })
+  .catch((err) => console.log(err));
